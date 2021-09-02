f67e9c3af1e49e394c16a94890c60fd660ebece8
alexius-schoolfnet <alexius@schoolfnet.com.br>
2021-09-02 10:33:33 -0300

    added CO on project

---
+++ pytransfer/funcoes.py
@@ -0,0 +1,21 @@
+
+# Funcoes
+
+import hashlib
+from random import choice, getrandbits
+
+
+def genBalanceRandom():
+    return getrandbits(16)
+
+
+def randomchar(tamanho):
+    # funcao para dandomizar um char de tamanho definido
+    caracteres = '123456-78ABCDEFGHJK-MNPQRSTXabcdefghjkmnpqrstxz-'
+    return ''.join(choice(caracteres) for _ in range(tamanho))
+
+
+def getnewtoken():
+    return hashlib.sha1(
+        (randomchar(5) + randomchar(5)).encode('utf-8')
+    ).hexdigest()
